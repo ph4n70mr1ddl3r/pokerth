0e261b83856e6d39391905b4a74f5afd57af8666
ph4n70mr1ddl3r <ph4n70mr1ddl3r@users.noreply.github.com>
2026-01-09 00:01:36 +0800

    Cleanup: Remove obsolete build files and translation sources
    - Remove old qmake .pro files (pokerth.pro, pokerth_db.pro, etc.)
    - Remove ts/ directory with translation source files (.ts)
    - Remove obsolete build scripts (run_astyle.sh, clean_build.sh, build_macos.sh, etc.)
    - Remove ChangeLog and TODO files (outdated documentation)
    
    The project now uses CMake for builds. Compiled translations are in data/translations/.

--- run_astyle.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-astyle --style=kr --indent=force-tab=4 -n --lineend=linux --exclude=src/third_party -r "src/*.h"
-astyle --style=kr --indent=force-tab=4 -n --lineend=linux --exclude=src/third_party -r "src/*.cpp"
